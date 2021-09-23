433dfdfebfc9fc2847c5a23e72426547b23d6aba
Antony Milne <antony.milne@quantumblack.com>
2021-09-23 12:46:48 +0100

    Update completed-kedro-inspect

--- completed-kedro-inspect/setup.py
+++ completed-kedro-inspect/setup.py
@@ -9,6 +9,6 @@ setup(
             "inspect_hooks = kedro_inspect.plugin:inspect_hooks",
             "time_dataset_loading_hooks = kedro_inspect.plugin:time_dataset_loading_hooks",
         ],
-        "kedro.project_commands": ["inspect_cli_hooks = kedro_inspect.cli_plugin:inspect_cli",],
+        "kedro.project_commands": "inspect_cli_hooks = kedro_inspect.cli_plugin:inspect_cli",
     },
 )
